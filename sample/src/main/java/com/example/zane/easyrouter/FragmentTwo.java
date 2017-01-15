c346753eca96976fd00d23ab0744d2322b4b3ce0
徐志 <zanebot96@gmail.com>
2017-01-15 19:57:28 +0800

    增加对onActivityResult()的回调hook

---
+++ sample/src/main/java/com/example/zane/easyrouter/FragmentTwo.java
@@ -0,0 +1,34 @@
+package com.example.zane.easyrouter;
+
+import android.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.zane.router.EasyRouter;
+
+/**
+ * Created by zane on 2017/1/15.
+ */
+
+public class FragmentTwo extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_two, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        view.findViewById(R.id.text_fragment_two).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EasyRouter.route(getActivity(), "activity://three");
+            }
+        });
+    }
+}
